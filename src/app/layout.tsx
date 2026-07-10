8dc988ee8e99271706aa8a764b16393840d4e77c
opcio0568 <gunwoo5758@gmail.com>
2026-07-10 11:10:01 +0900

    Suppress extension hydration warning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: ReactNode;
 }>) {
   return (
-    <html lang="ko">
+    <html lang="ko" suppressHydrationWarning>
       <body>{children}</body>
     </html>
   );
